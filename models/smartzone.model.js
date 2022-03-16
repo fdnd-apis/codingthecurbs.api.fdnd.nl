549e8f208658da571a3409cb221df001abf8e691
ju5tu5 <justus@ju5tu5.nl>
2022-03-16 13:22:48 +0100

    Update smartzone.model.js

--- models/smartzone.model.js
+++ models/smartzone.model.js
@@ -173,7 +173,8 @@ function prepareForPut(smartzone) {
  */
 function prepareForPatchQuery(smartzone) {
   delete smartzone.smartzoneId
-  console.log(smartzone.getOwnPropertyNames)
+  console.log(smartzone)
+  console.log(Object.getOwnPropertyNames(smartzone))
   return smartzone.getOwnPropertyNames.reduce((prev, curr) => `${prev}, ${curr} = ?`)
 }
 
